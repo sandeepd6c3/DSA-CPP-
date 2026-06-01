a8d328080df4f202d64699a3dc03b67661c92c77
[Sandeep] <sandeep977244@gmail.com>
2026-06-01 08:38:11 +0530

    Tilda (~) , XOR , and left shift operator !

---
+++ BitwiseOperators/operators.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	// bool a = false;
+	// bool b = true;
+	
+	// cout<<~a;
+	
+	// cout<<~10;
+	
+	// cout<<(a^b);
+	
+	int x = 10;
+	
+	x = x << 1;
+	
+	cout<<x<<endl;
+	
+	return 0;
+}
